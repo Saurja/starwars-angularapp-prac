59d39829de5a391e6e37e05fdfe8be8f717f5069
Saurja Ghosh <saurjaghoshofficial@gmail.com>
2021-06-08 21:58:11 +0530

    App Made

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { StarWarsService } from "app/star-wars.service";
+
+@Component({
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"],
+})
+export class ListComponent implements OnInit {
+  characters = [];
+  activatedRoute: ActivatedRoute;
+  swService: StarWarsService;
+  loadedSide = "all";
+  subscription;
+
+  constructor(activatedRoute: ActivatedRoute, swService: StarWarsService) {
+    this.activatedRoute = activatedRoute;
+    this.swService = swService;
+  }
+
+  ngOnInit() {
+    this.swService.fetchCharacters();
+    this.activatedRoute.params.subscribe((params: any) => {
+      this.characters = this.swService.getCharacters(params.side);
+      this.loadedSide = params.side;
+    });
+
+    this.subscription = this.swService.charactersChanged.subscribe(() => {
+      this.characters = this.swService.getCharacters(this.loadedSide);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
